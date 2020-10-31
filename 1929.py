2d22b401481df5221b8ccf8d37a585d56122fc98
jeongminhui99 <63187100+jeongminhui99@users.noreply.github.com>
2020-11-01 03:12:52 +0900

    Add Python File [1929.py] 소수 구하기

--- 1929.py
+++ 1929.py
@@ -3,6 +3,7 @@ for i in range(2, 1000001):
     if n_list[i]:
         for j in range(i*2, 1000001, i):
             n_list[j] = False
+            
 n, m = map(int,input().split())
 for i in range(n,m+1):
     if n_list[i]:
